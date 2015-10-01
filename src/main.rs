01c205a0897b368a70ec0c1fce94b96b1c27fe88
m <m@marvelmathew.ca>
2015-09-30 22:34:03 -0500

    Record last message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 extern crate irc;
+extern crate time;
 
 use std::default::Default;
 use irc::client::prelude::*;
 
+use time::SteadyTime;
+
+use std::cell::RefCell;
+
+struct LastMessage {
+    time: SteadyTime
+}
+
 fn main() {
     let server = IrcServer::new("config.json").unwrap();
-    for message in server.iter_cmd() {
+    let last_message = RefCell::new(LastMessage{time: SteadyTime::now()});
 
+    for command in server.iter_cmd() {
+        match command.unwrap() {
+            cmd@Command::PRIVMSG(target, raw_message) => {
+                let msg: Message = cmd.into();
+                // Only if channel message
+                if msg.get_source_nickname().is_none() {
+                    last_message.borrow_mut().time = SteadyTime::now();
+                }
+            },
+            _ =>
+                println!("something else"),
+        }
     }
 }
